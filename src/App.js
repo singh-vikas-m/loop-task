12b6e1cca3f2c71c81e011a1199c2cea48c3be49
Singh-Vikas-M <vikas3091999@gmail.com>
2021-11-09 21:38:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Tiptap from "./Components/Tiptap/Tiptap";
+import Sidenav from "./Components/Sidenav/Sidenav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidenav />
+      <div style={{ width: "100%" }}>
+        <Tiptap />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Sidenav/Sidenav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Sidenav.scss";
+import logo from "../../Asset/loop-logo.svg";
+function App() {
+  return (
+    <div className="Sidenav">
+      <img src={logo} alt="logo" />
+    </div>
+  );
+}
+
+export default App;
